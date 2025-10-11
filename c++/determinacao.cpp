ab5c76fd7d5e2bc1a7909b5669dbe1b12c0e3122
Gabriel Spíndola <gabrielhspindola@gmail.com>
2025-10-10 22:34:55 -0300

    Add files via upload

---
+++ c++/determinacao.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<vector<int>> A, G, Gt;
+int n;
+
+int calcula_Gij(int, int);
+vector<vector<int>> transposta(vector<vector<int>>);
+void imprimir(vector<vector<int>>);
+
+int main(){
+    cin >> n;
+
+    A.resize(n, vector<int>(n));
+    G.resize(n, vector<int>(n, 0));
+
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < n; j++)
+            cin >> A[i][j];
+
+    cout << "Matriz A:" << endl;
+    imprimir(A);
+    cout << endl;
+
+    //Calcula a matriz G
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < n; j++)
+            G[i][j] = calcula_Gij(i, j);
+    
+    //Determina Gt
+    Gt = transposta(G);
+
+    //imprimi os resultados
+    cout << "Matriz G:" << endl;
+    imprimir(G);
+    cout << endl;
+
+    cout << "Matriz Gt:" << endl;
+    imprimir(Gt);
+    cout << endl;
+
+    return 0;
+}
+
+//Calcula a componente Gij
+int calcula_Gij(int i, int j){
+    //TODO
+    return 0;
+}
+
+void imprimir(vector<vector<int>> mat){
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++)
+            printf("%2d ", mat[i][j]);
+        cout << endl;
+    }
+}
+
+vector<vector<int>> transposta(vector<vector<int>> mat){
+    vector<vector<int>> T(n, vector<int>(n));
+
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < n; j++)
+            T[i][j] = mat[j][i];
+
+    return T;
+}
