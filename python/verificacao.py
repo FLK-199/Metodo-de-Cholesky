3328073e84fe9d87eea1de8653797ede934b789f
Gabriel Spíndola <gabrielhspindola@gmail.com>
2025-10-15 16:15:30 -0300

    Update verificacao.py

--- python/verificacao.py
+++ python/verificacao.py
@@ -9,10 +9,19 @@ for i in range(n):
 
 if np.array_equal(A, A.T):
     print("e simetrica")
+    f1 = 0
 else:
     print("nao e simetrica")
+    f1 = 1
 
 if np.all(np.linalg.eigvals(A) > 0):
     print("e definida positiva")
+    f2 = 0
 else:
     print("nao e definida positiva")
+    f2 = 1
+
+if f1 == 0 and f2 == 0:
+    print("pode ser aplicado o metodo de Cholesky")
+else:
+    print("nao pode ser apicado o metodo de Cholesky")
