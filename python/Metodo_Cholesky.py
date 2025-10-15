fbff1d9f23611f03d3178aee76dc974d8f2966d7
Gabriel Spíndola <gabrielhspindola@gmail.com>
2025-10-15 16:27:51 -0300

    Update Metodo_Cholesky.py

---
+++ python/Metodo_Cholesky.py
@@ -0,0 +1,64 @@
+import numpy as np
+import math
+
+n = int(input())
+
+A = np.zeros((n, n))
+for i in range(n):
+    A[i] = list(map(float, input().split()))
+    
+b = list(map(float, input().split()))
+
+def verificacao():
+    if np.array_equal(A, A.T):
+        print("e simetrica")
+        f1 = 0
+    else:
+        print("nao e simetrica")
+        f1 = 1
+
+    if np.all(np.linalg.eigvals(A) > 0):
+        print("e definida positiva")
+        f2 = 0
+    else:
+        print("nao e definida positiva")
+        f2 = 1
+
+    if f1 == 0 and f2 == 0:
+        print("pode ser aplicado o metodo de Cholesky")
+        return 0
+    else:
+        print("nao pode ser apicado o metodo de Cholesky")
+        return 1
+
+def imprimir(mat):
+    for i in mat:
+        print(i)
+    print()
+
+def calculo_G(mat): 
+    G = np.zeros((n, n))
+
+    for j in range(n):
+        for i in range(j, n):
+            if i == 0 and j == 0:
+                G[0][0] = math.sqrt(A[0][0])
+            if i == j:
+                soma = sum(G[i][k]**2 for k in range(i))
+                G[i][i] = math.sqrt(A[i][i] - soma)
+            elif j == 0:
+                G[i][0] = A[i][0] / G[0][0]
+            else:
+                soma = sum(G[i][k] * G[j][k] for k in range(j))
+                G[i][j] = (A[i][j] - soma) / G[j][j]
+
+    return G
+
+if verificacao() == 0:
+    G = calculo_G(A)
+    Gt = G.T
+    y = np.linalg.solve(G,b)
+    x = np.linalg.solve(Gt,y)
+
+    print("x:")
+    print(x)
