6f35763b9c97a849c791f8fe321e8388fad23a15
Gabriel Spíndola <gabrielhspindola@gmail.com>
2025-10-15 16:12:20 -0300

    Update verificacao.py

--- python/verificacao.py
+++ python/verificacao.py
@@ -1,1 +1,18 @@
+import numpy as np
+import math
 
+n = int(input())
+A = np.zeros((n, n))
+
+for i in range(n):
+    A[i] = list(map(float, input().split()))
+
+if np.array_equal(A, A.T):
+    print("e simetrica")
+else:
+    print("nao e simetrica")
+
+if np.all(np.linalg.eigvals(A) > 0):
+    print("e definida positiva")
+else:
+    print("nao e definida positiva")
